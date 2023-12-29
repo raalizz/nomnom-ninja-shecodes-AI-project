d53150ee90bfc392bcabac050e28bf6027ac175d
raalizz <135476716+raalizz@users.noreply.github.com>
2023-12-29 13:57:08 +0100

    I added ids on the form and on the recipe and added the basic js form handling and a typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "the recipe will go here",
+    autoStart: true,
+    cursor: "🧑🏻‍🍳",
+    delay: 40,
+  });
+}
+
+let formElement = document.querySelector("#form");
+formElement.addEventListener("submit", generateRecipe);

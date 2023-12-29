ca5d61aeb320a09e2bf995523e15b486a83e809f
raalizz <135476716+raalizz@users.noreply.github.com>
2023-12-29 19:18:16 +0100

    I added the API integration to make it fully functional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 function generateRecipe(event) {
   event.preventDefault();
 
+  let ingredientsInput = document.querySelector("#ingredients");
+
+  let apiKey = "da1f0bo0fb43abc333bbad06624a9e8t";
+  let prompt = `User instructions: Generate one recipe only from the ${ingredientsInput.value} in basic HTML and separate each line with a <br /> . Make the recipe name bold and underlined.`;
+  let context =
+    "You are a professional chef and love to create delicious and simple recipes using any ingredient available in the kitchen. Your mission is to generate full step-by-step simple recipes with the provided ingredients. Make sure to follow the user's instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
+}
+
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "the recipe will go here",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "🧑🏻‍🍳",
-    delay: 40,
+    delay: 10,
   });
 }
 
